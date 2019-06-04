5040331a9c68a2b79d10557379ae5bb4b3f63cfc
Crystal <hello@crystalbohacek.com>
2019-06-04 17:48:05 -0400

    fetching episode data

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import React from "react";
 import { Store } from './Store'
 
 export default function App(): JSX.Element {
-  const store = React.useContext(Store)
+  const URL = 
+    "https://api.tvmaze.com/singlesearch/shows?q=the-office&embed=episodes";
+  const {state, dispatch} = React.useContext(Store)
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction()
+  })
+  const fetchDataAction = async () => {
+    const data = await fetch(URL)
+    const dataJSON = await data.json()
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    })
+  }
+  console.log(state)
   return (
     <React.Fragment>
-      {console.log(store)}
       <h1>The Office</h1>
       <p>Pick your favorite episode!</p>
     </React.Fragment>
