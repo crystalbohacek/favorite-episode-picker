9dfff96299d6ccc1c65138adc105e00c6ad16ecf
Crystal <hello@crystalbohacek.com>
2019-06-04 18:56:35 -0400

    display api data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { Store } from './Store'
 
+interface IEpisode {
+  airdate: string,
+  id: number,
+  image: { medium: string, original: string },
+  name: string,
+  number: number,
+  season: number,
+  summary: string,
+  url: string
+}
+
 export default function App(): JSX.Element {
   const URL = 
     "https://api.tvmaze.com/singlesearch/shows?q=the-office&embed=episodes";
@@ -16,11 +27,26 @@ export default function App(): JSX.Element {
       payload: dataJSON._embedded.episodes
     })
   }
-  console.log(state)
   return (
     <React.Fragment>
       <h1>The Office</h1>
       <p>Pick your favorite episode!</p>
+      <section>
+        {state.episodes.map((episode: IEpisode) => {
+          return (
+            <section key={episode.id}>
+              <img
+                alt={`The Office ${episode.name}`}
+                src={episode.image && episode.image.medium}
+              />
+              <h3>{episode.name}</h3>
+              <section>
+                Season: {episode.season} Number: {episode.number}
+              </section>
+            </section>
+          );
+        })}
+      </section>
     </React.Fragment>
   );
 }
